5bd268dbb7eded51841e8aeaf14d61a74ab793ec
Alex Olivas <alex.olivas11@gmail.com>
2020-08-12 16:09:41 -0500

    temp commit containing shell of the next script I will be writing

---
+++ create_project.py
@@ -0,0 +1,8 @@
+# This script contains code to create common software development projects automating steps that I always have to run
+# e.g. for react projects build project with create-react-app, install eslint, create .eslintrc file, etc etc
+# same for django projects
+# An example video for someting I could do https://www.youtube.com/watch?v=GcZOaEivf_c
+
+
+# Create a script that deletes screenshots after a day
+# Notify user is miscallaneous folder is not empty, this prompts them to go in and clean up their directory
