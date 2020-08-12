5c3fd744f430a387255a2ce2ae8075b9023ea797
Alex Olivas <alex.olivas11@gmail.com>
2020-08-12 13:02:55 -0500

    added watchdog to listen for events on the desktop

--- desktop_watcher.py
+++ desktop_watcher.py
@@ -1,16 +1,7 @@
-"""
-#!/usr/bin/env python
-Requirements:
-1) Should watch for file changes in the desktop in the background at all times
-2) It should move files into the user's directory in the following way:
-    - if screenshot move it into ~/Documents/Screenshots
-    - else move into ~/Documents/<file-type> e.g word doc is moved to ~/Documents/word, png, gif, etc into ~/Documents/Pictures
-3) Stretch Goal: remove screenshots that have not been renamed after X number of days (from the new screenshots directory)
-4) Stretch Goal: should be robust enough to work in any OS (for now just make it work on a mac)
-"""
 import imghdr
 import os
 import shutil
+import time
 
 from pathlib import Path
 
@@ -19,66 +10,99 @@ from watchdog.events import FileSystemEventHandler
 
 
 home_dir = str(Path.home())
-documents_dir = f"{home_dir}/Documents"
-print(f"Home directory: {home_dir}")
-print(f"Documents directory: {documents_dir}")
+desktop_dir = f"{home_dir}/DESKTOP"
 
-sort_rules = {
-    "image": "Images",
-    "misc": "Miscellaneous",
-    "screen_shot": "Screenshots",
-}
 
-def create_sort_directories():
+class DesktopWatchHandler(FileSystemEventHandler):
     """
-    This method is responsible for creating all the directories our
-    script will be sorting any file created on the desktop
+    Watches for file system events on the user's desktop and when anything is
+    modified on there this handler sorts whatever is created into a specific
+    folder on their documents directory to help keep the desktop clutter free.
     """
-    # TODO: we could make this configurable somehow, look into this for the next version
-    # TODO: This should also be a key/value pair so that the rules and directories can be dynamic
-    for sort_directory in sort_rules.values():
-        # TODO Make the base directory configurable
-        new_dir = f"{home_dir}/Documents/{sort_directory}"
-        Path(new_dir).mkdir(parents=True, exist_ok=True)
 
-def sort_desktop_files():
-    """
-    This method is responsible for sorting any file created on the
-    desktop into a directory based on pre-configured rules
-    """
-    ignore_files = [".DS_Store", ".localized"]
-    desktop_dir = f"{home_dir}/DESKTOP"
+    def __init__(self):
+        super(FileSystemEventHandler, self).__init__()
 
-    for filename in os.listdir(desktop_dir):
-        if filename not in ignore_files:
-            # The default destination location is the "miscellaneous" directory
-            dest_dir = sort_rules["misc"]
+        # TODO: This could be made configurable at some point
+        self.sort_rules = {
+            "image": "Images",
+            "misc": "Miscellaneous",
+            "screen_shot": "Screenshots",
+        }
 
-            src_path = f"{desktop_dir}/{filename}"
-            is_directory = os.path.isdir(src_path)
+        self.__create_sort_directories()
 
-            if os.path.islink(src_path):
-                # Symlinks are to be ignored
-                continue
-            elif filename.startswith("Screen Shot"):
-                dest_dir = sort_rules["screen_shot"]
-            elif not is_directory:
-                # When this path is not a directory, check if it is a valid
-                # image and if so, send it to the "image" directory
-                image_file_type = imghdr.what(src_path)
-                if image_file_type is not None:
-                    dest_dir = sort_rules["image"]
+    def on_modified(self, event):
+        """
+        Called when a file or directory is modified.
 
-            # TODO: add logic to rename a file if it already exists
+        :param event:
+            Event representing file/directory modification.
+        :type event:
+            :class:`DirModifiedEvent` or :class:`FileModifiedEvent`
+        """
+        if desktop_dir == event.src_path:
+            # Just an additional security check that ensures that the logic to sort
+            # any new desktop files/directories only runs when a modification is made
+            # on the user's desktop
+            self.__sort_desktop_files()
 
-            # Actually move the file or directory
-            dest_path = f"{documents_dir}/{dest_dir}/{filename}"
-            shutil.move(src_path, dest_path)
+    def __create_sort_directories(self):
+        """
+        This method is responsible for creating all the directories our
+        script will be sorting any file created on the desktop
+        """
+        # TODO: we could make this configurable somehow, look into this for the next version
+        # TODO: This should also be a key/value pair so that the rules and directories can be dynamic
+        for sort_directory in self.sort_rules.values():
+            # TODO Make the base directory configurable, don't create if it already exists
+            new_dir = f"{home_dir}/Documents/{sort_directory}"
+            Path(new_dir).mkdir(parents=True, exist_ok=True)
+
+    def __sort_desktop_files(self):
+        """
+        This method is responsible for sorting any file or directory created on the
+        desktop into a directory based on pre-configured rules
+        """
+        ignore_files = [".DS_Store", ".localized"]
+        desktop_dir = f"{home_dir}/DESKTOP"
+        documents_dir = f"{home_dir}/Documents"
+
+        for filename in os.listdir(desktop_dir):
+            if filename not in ignore_files:
+                # The default destination location is the "miscellaneous" directory
+                dest_dir = self.sort_rules["misc"]
+
+                src_path = f"{desktop_dir}/{filename}"
+                is_directory = os.path.isdir(src_path)
+
+                if os.path.islink(src_path):
+                    # Symlinks are to be ignored
+                    continue
+                elif filename.startswith("Screen Shot"):
+                    dest_dir = self.sort_rules["screen_shot"]
+                elif not is_directory:
+                    # When this path is not a directory, check if it is a valid
+                    # image and if so, send it to the "image" directory
+                    image_file_type = imghdr.what(src_path)
+                    if image_file_type is not None:
+                        dest_dir = self.sort_rules["image"]
+
+                # TODO: add logic to rename a file if it already exists
+
+                # Actually move the file or directory
+                dest_path = f"{documents_dir}/{dest_dir}/{filename}"
+                shutil.move(src_path, dest_path)
 
 
-# class DesktopWatcher(FileSystemEventHandler):
-#     pass
-
-
-create_sort_directories()
-sort_desktop_files()
+if __name__ == "__main__":
+    desktop_watch_handler = DesktopWatchHandler()
+    observer = Observer()
+    observer.schedule(desktop_watch_handler, desktop_dir, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(10)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
